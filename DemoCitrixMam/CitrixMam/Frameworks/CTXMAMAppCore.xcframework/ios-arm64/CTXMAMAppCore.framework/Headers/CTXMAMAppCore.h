612c545c064e2db1eb8d9c50379622fb2bbb32e3
Raphael Guye <raphael.guye@gmail.com>
2021-10-20 11:09:03 +0200

    Integrate MAM sdk

---
+++ DemoCitrixMam/CitrixMam/Frameworks/CTXMAMAppCore.xcframework/ios-arm64/CTXMAMAppCore.framework/Headers/CTXMAMAppCore.h
@@ -0,0 +1,21 @@
+//
+//  CTXMAMAppCore.h
+//  CTXMAMAppCore
+//
+//  Citrix iOS MAM SDK Public Header
+//  Copyright © 2020-2021 Citrix Systems, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CTXMAMAppCore.
+FOUNDATION_EXPORT double CTXMAMAppCoreVersionNumber;
+
+//! Project version string for CTXMAMAppCore.
+FOUNDATION_EXPORT const unsigned char CTXMAMAppCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CTXMAMAppCore/PublicHeader.h>
+
+@interface CTXMAMAppCore : NSObject
+
+@end
---
+++ DemoCitrixMam/DemoCitrixMam-Bridging-Header.h
@@ -0,0 +1,1 @@
+#import <CTXMAMCore/CTXMAMCore.h>
