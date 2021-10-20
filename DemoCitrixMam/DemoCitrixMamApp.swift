2a072bb00e18ab673f4e084010c61ea6e59a6e48
Raphael Guye <raphael.guye@gmail.com>
2021-10-20 11:01:08 +0200

    Initial Commit

---
+++ DemoCitrixMam/DemoCitrixMamApp.swift
@@ -0,0 +1,15 @@
+//
+// Created by Raphael Guye on 20.10.21
+// https://github.com/raphaelguye
+// 
+
+import SwiftUI
+
+@main
+struct DemoCitrixMamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
